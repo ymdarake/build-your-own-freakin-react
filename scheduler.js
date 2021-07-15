23fee64c957403afac6248fb78e4d0976d5153e7
ymdarake <t.sakamoto.ouen@gmail.com>
2021-07-15 17:52:43 +0900

    [step 3]Concurrent Mode

---
+++ scheduler.js
@@ -0,0 +1,18 @@
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+	let shouldYield = false;
+
+	while (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+		shouldYield = deadline.timeRemaining() < 1;
+	}
+
+	requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {
+	// TODO:
+}
