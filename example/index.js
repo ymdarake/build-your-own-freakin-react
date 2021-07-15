63e46a79a13fe91eee176622e5a48141d49dc8ae
ymdarake <t.sakamoto.ouen@gmail.com>
2021-07-15 20:04:48 +0900

    [update]example

--- example/index.js
+++ example/index.js
@@ -10,17 +10,13 @@ Didact.render(
   document.getElementById('root')
 );
 
-Didact.run();
+// /** @jsx Didact.createElement */
+// function Counter() {
+//   const [state, setState] = Didact.useState(1);
+//   return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+// }
+// const element = <Counter />;
+// const container = document.getElementById('root');
+// Didact.render(element, container);
 
-setTimeout(() => {
-  console.warn('re-rendering...');
-  Didact.render(
-    Didact.createElement(
-      'div',
-      { id: 'foo', onClick: console.log },
-      Didact.createElement('strong', null, 'bar'),
-      Didact.createElement('b')
-    ),
-    document.getElementById('root')
-  );
-}, 3000);
+Didact.run();
