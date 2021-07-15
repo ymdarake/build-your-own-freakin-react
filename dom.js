1f82a16047783c286276ca1ee65ff515336032db
ymdarake <t.sakamoto.ouen@gmail.com>
2021-07-15 18:23:07 +0900

    [step 4]Fiber

---
+++ dom.js
@@ -0,0 +1,39 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createDom(fiber) {
+  const dom =
+    fiber.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type);
+
+  Object.keys(fiber.props)
+    .filter((key) => key !== 'children')
+    .forEach((key) => (dom[key] = fiber.props[key]));
+  return dom;
+}
+
+module.exports = {
+  createElement,
+  createTextElement,
+  createDom,
+};
--- index.js
+++ index.js
@@ -1,44 +1,89 @@
-function createElement(type, props, ...children) {
-	return {
-		type,
-		props: {
-			...props,
-			children: children.map(child => typeof child === 'object' ? child : createTextElement(child))
-		}
-	}
-}
-
-function createTextElement(text) {
-	return {
-		type: "TEXT_ELEMENT",
-		props: {
-			nodeValue: text,
-			children: []
-		}
-	}
-}
+const DidactDOM = require('./dom');
 
 function render(element, container) {
-	const dom = element.type  === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
 
-	Object.keys(element.props).filter(key => key !== "children").forEach(key => dom[key] = element.props[key]);
-	element.props.children.forEach(child => render(child, dom));
+let nextUnitOfWork = null;
 
-	container.appendChild(dom);
+function workLoop(deadline) {
+  console.warn('workLoop!');
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = DidactDOM.createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers for children
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      propr: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    ++index;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const Didact = {
-	createElement,
-	render
-}
+  createElement: DidactDOM.createElement,
+  render,
+};
 
 const element = Didact.createElement(
-	"div",
-	{ id: "foo"},
-	Didact.createElement("a", null, "bar"),
-	Didact.createElement("b")
-)
+  'div',
+  { id: 'foo' },
+  Didact.createElement('a', null, 'bar'),
+  Didact.createElement('b')
+);
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 
 Didact.render(element, container);
